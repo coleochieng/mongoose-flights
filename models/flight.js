a22a58e0ad153e033da461e6d98a18b9abfc222f
coleochieng <nicole.ochieng98@gmail.com>
2022-07-20 18:36:36 -0500

    update models > flight.js

--- models/flight.js
+++ models/flight.js
@@ -1,5 +1,27 @@
 const mongoose = require('mongoose');
 
-const Schema = {
-    testing
-}
+const Schema = mongoose.Schema;
+
+destinationSchema = new Schema ({
+    airline: {
+        type: String,
+        enum: ['American', 'Southwest', 'United'],
+    },
+    airport: {
+        type: String,
+        enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
+        default: 'DEN',
+    },
+    flightNo: {
+        type: Number,
+        min: 10,
+        max: 9999,
+    },
+    departs: {
+        type: Date,
+        default: function(){
+            let today = new Date();
+            return today.setFullYear(today.getFullYear() + 1);
+        },
+    },
+})
